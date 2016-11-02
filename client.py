93062e68930fa9272bb59a73198d459ffdc54450
rgtjf <rgtjf1@163.com>
2016-11-02 12:46:36 +0800

    update repo

--- client.py
+++ client.py
@@ -1,18 +1,36 @@
 # coding: utf-8
 from  __future__ import print_function
 import pyjsonrpc
-import json
 
-http_client = pyjsonrpc.HttpClient(
-    url = "http://localhost:8083",
-)
+class Embedding(object):
+
+    def __init__(self):
+        self.http_client = pyjsonrpc.HttpClient(
+            url = "http://localhost:8083",
+        )
+
+    def get_word2vec(self, word):
+        vec = self.http_client.word2vec(word)
+        return vec
+
+    def get_glove(self):
+        vec = self.http_client.glove(word)
+        return vec
+
+embedding = Embedding()
+
+if __name__ == '__main__':
+
+    http_client = pyjsonrpc.HttpClient(
+        url="http://localhost:8083",
+    )
 
 
-c = http_client.add(1, 2)
-print(c, type(c))
+    c = http_client.add(1, 2)
+    print(c, type(c))
 
-word2vec = http_client.word2vec("cat")
-print(word2vec)
+    word2vec = http_client.word2vec("cat")
+    print(word2vec, len(word2vec[1]))
 
-glove = http_client.glove("cat")
-print(glove)
+    glove = http_client.glove("cat")
+    print(glove)
--- server.py
+++ server.py
@@ -32,14 +32,21 @@ def _load_w2v(in_file):
                     break
                 if ch != '\n':
                     word.append(ch)
-            embeddings[word] = list(np.fromstring(f.read(binary_len)))
+            embeddings[word] = np.fromstring(f.read(binary_len), dtype='float32').tolist()
             # print(word, embeddings[word])
     return embeddings
 
-w2v_file = 'PATH TO GoogleNews-vectors-negative300.bin'
-glove_file = 'PATH TO glove.6B.100d.txt'
+''' input file path '''
+w2v_file = '/home/junfeng/word2vec/GoogleNews-vectors-negative300.bin'
+glove_file = '/home/junfeng/GloVe/glove.6B.100d.txt'
+
+''' word2vec embedding dict and unk word embedding dict '''
 word2vec_embedding = _load_w2v(w2v_file)
+word2vec_unk_embedding = {}
+
+''' glove embedding dict and unk word embedding dict '''
 glove_embedding = _load_glove(glove_file, 100)
+glove_unk_embedding = {}
 
 class Word2VecRequestHandler(pyjsonrpc.HttpRequestHandler):
 
@@ -49,13 +56,17 @@ class Word2VecRequestHandler(pyjsonrpc.HttpRequestHandler):
 
     @pyjsonrpc.rpcmethod
     def word2vec(self, word):
-        range = (-0.01, 0.01)
+        range = (-0.25, 0.25)
         shape = (300,)
         if word in word2vec_embedding:
-            vec = word2vec_embedding[word]
+            vec = ('in', word2vec_embedding[word])
+        elif word in word2vec_unk_embedding:
+            vec = ('unk', word2vec_unk_embedding[word])
         else:
             vec = list(np.random.uniform(low=range[0], high=range[1], size=shape))
-            word2vec_embedding[word] = vec
+            word2vec_unk_embedding[word] = vec
+            vec = ('unk', vec)
+        print('query word2vec', word, vec[0])
         return vec
 
     @pyjsonrpc.rpcmethod
@@ -63,10 +74,14 @@ class Word2VecRequestHandler(pyjsonrpc.HttpRequestHandler):
         range = (-1, 1)
         shape = (100,)
         if word in glove_embedding:
-            vec = glove_embedding[word]
+            vec = ('in', glove_embedding[word])
+        elif word in glove_unk_embedding:
+            vec = ('unk', glove_unk_embedding[word])
         else:
             vec = list(np.random.uniform(low=range[0], high=range[1], size=shape))
-            glove_embedding[word] = vec
+            glove_unk_embedding[word] = vec
+            vec = ('unk', glove_unk_embedding[word])
+        print('query glove', word, vec[0])
         return vec
 
 if __name__ == '__main__':
